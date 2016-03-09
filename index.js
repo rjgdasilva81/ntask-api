dc6510fb98450bb6159e8cf97032cdf41c9d4e45
Robson Galvao Silva <rjgdasilva@gmail.com>
2016-03-09 01:02:44 -0300

    Inclusão de plugin preset ES5 e do modulo cosign

--- index.js
+++ index.js
@@ -3,5 +3,15 @@ const PORT = 3000;
 
 const app = exp();
 
+app.set("json spaces",4);
 app.get("/", (req,res) => res.json({status: "NTask API"}));
+
+app.get("/tasks" , (req,res) => {
+  res.json({
+	 tasks: [
+		 {title: "Fazer compras"},
+		 {title: "Consertar o pc"},
+	 ]
+  });
+});
 app.listen(PORT,() => console.log(`NTask API - porta ${PORT}`));
